33a611a7ed0c13c2cdc39f0a1556c459575d13c8
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-10-18 10:51:38 -0700

    Added some extra alerts for each individual question to allow a clearer view of the correct and wrong answers

--- app.js
+++ app.js
@@ -2,31 +2,62 @@
 
 alert('The following questions are yes or no, please answer Y or N.');
 
+// First Question.
 var dogs = prompt('Does Dan like dogs?');
-console.log('Dan number of dogs:', dogs);
+console.log('Dan likes dogs:', dogs);
 
+// Second Question.
 var hats = prompt('Does Dan wear hats?');
-console.log('Dan owns this many hats:', hats);
+console.log('Dan wears hats:', hats);
 
+// Third Question.
 var games = prompt('Does Dan like video games?');
-console.log('Dan owns how many games:', games);
+console.log('Dan likes games:', games);
 
+// Fourth Question.
 var veteran = prompt('Is Dan a veteran?');
-console.log('Dan is how old?', veteran);
+console.log('Dan is a veteran:', veteran);
 
+// Fifth Question.
 var beer = prompt('Do you think I like beer?');
 console.log('Dan likes beer:', beer);
 
-dogs.toUpperCase();
-hats.toUpperCase();
-games.toUpperCase();
-veteran.toUpperCase();
-beer.toUpperCase();
-
-if (dogs === 'Y' && hats === 'N' && games === 'Y' && veteran === 'Y' && beer === 'Y') {
-  alert('Awesome! You know me very well!');
-} else if (dogs === 'Y' || hats === 'N' || games === 'Y' || veteran === 'Y' || beer === 'Y') {
-  alert('You\'re Partially Right!');
+// Validation Conditional Statements: This code verifies whether the person got all answers, some answers, or no answers correct.
+if (dogs.toLowerCase() === 'y' && hats.toLowerCase() === 'n' && games.toLowerCase() === 'y' && veteran.toLowerCase() === 'y' && beer.toLowerCase() === 'y') {
+  alert('Awesome! you know me very well!');
+} else if (dogs.toLowerCase() === 'y' || hats.toLowerCase() === 'n' || games.toLowerCase() === 'y' || veteran.toLowerCase() === 'y' || beer.toLowerCase() === 'y') {
+  alert('you\'re Partially Right!');
 } else {
-  alert('You don\'t know me very well');
+  alert('you don\'t know me very well');
+}
+
+// Validation Conditional Statements: This code verifies each individual answer to determine which answers were correctly answered and which weren't.
+if (dogs.toLowerCase() === 'y') {
+  alert('Yeah, Dan does like dogs!');
+} else {
+  alert('Nah, Dan does infact like dogs.');
+}
+
+if (hats.toLowerCase() === 'n') {
+  alert('Correct, Dan doesn\'t usually wear hats.');
+} else {
+  alert('Nah, Dan owns hats but wears none.');
+}
+
+if (games.toLowerCase() === 'y') {
+  alert('Heck ya! Dan\'s a gamer!');
+} else {
+  alert('Dan actually does like games.');
+}
+
+if (veteran.toLowerCase() === 'y') {
+  alert('Dan was a soldier for sure.');
+} else {
+  alert('Incorrect, Dan was in the Army.');
+}
+
+if (beer.toLowerCase() === 'y') {
+  alert('Duh, Beer is tasty!');
+} else {
+  alert('Wrong, Beer is super diverse and great.');
 }
